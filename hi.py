a81497da9ec9c666f083979a33144e667f2e5a48
RishabhMakes <rishabhisar@gmail.com>
2020-01-25 16:48:45 +0530

    Fixed final output issue, infinite scroll seems to work too

--- hi.py
+++ hi.py
@@ -5,19 +5,42 @@ from selenium.webdriver.common.keys import Keys
 import time
 opts = webdriver.ChromeOptions()
 opts.binary_location = "D:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+prefs = {"profile.managed_default_content_settings.images": 2}
+opts.add_experimental_option("prefs", prefs)
 driver = webdriver.Chrome(chrome_options = opts)
+
 driver.get('https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin')
 username = driver.find_element_by_id('username')
-username.send_keys('iitbestinstitute@gmail.com')
+username.send_keys('rishabhisar@gmail.com')
 password = driver.find_element_by_id('password')
-password.send_keys('iloveiit' + Keys.RETURN)
+password.send_keys('puru@1997' + Keys.RETURN)
 ## submit = driver.find_elements_by_class_name('btn__primary--large from__button--floating')
 ##submit = driver.find_elements_by_tag_name('button')
 ##submit.click()
 driver.get('https://www.linkedin.com/school/indian-institute-of-technology-bombay/people/')
 time.sleep(5)
 
+##======================THIS IS THE SCROLLING FUNCTION=========================================
 
+# Get scroll height
+last_height = driver.execute_script("return document.body.scrollHeight")
+
+while True:
+    # Scroll down to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight+1000);")
+
+    # Wait to load page
+    time.sleep(2)
+
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
+
+
+
+##============THIS PART PARSES THE PAGE AND PRINTS OT THE LINKS===============================
 
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
@@ -29,7 +52,12 @@ print(type(soup))
 ##print(linkedin_urls[0])
 
 ##print(soup.find_all('a')[19].attrs)
-
-print(soup.find_all('a')[19]['href'])
+items = soup.find_all('a')
+print(len(items))
+print(items[19]['href'])
 print('=====================================================')
-print(link['href'] for link in soup.find_all('a'))
+print(items[20]['href'])
+print('=====================================================')
+for link in items:print(link['href'])
+
+##================TILL HERE ==========================================================
