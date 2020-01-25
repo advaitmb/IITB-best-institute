14da3550b76a43f3809f23accce37d77f98bc097
Advait Bhat <advaitmb@gmail.com>
2020-01-25 17:07:27 +0530

    Added Scroll

--- hi.py
+++ hi.py
@@ -3,20 +3,53 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import random
+
+#lists
+list_names = []
+list_position = []
+list_batch = []
+list_link = []
+
+# A randomizer for the delay
+seconds = 5 + (random.random() * 2)
+
+#Initiate Chrome for Linkedin login
 opts = webdriver.ChromeOptions()
-opts.binary_location = "D:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+opts.binary_location = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
 driver = webdriver.Chrome(chrome_options = opts)
 driver.get('https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin')
+
+#username
 username = driver.find_element_by_id('username')
-username.send_keys('iitbestinstitute@gmail.com')
+username.send_keys('rishabhisar@gmail.com')
+
+#password
 password = driver.find_element_by_id('password')
-password.send_keys('iloveiit' + Keys.RETURN)
+password.send_keys('puru@1997' + Keys.RETURN)
 ## submit = driver.find_elements_by_class_name('btn__primary--large from__button--floating')
 ##submit = driver.find_elements_by_tag_name('button')
 ##submit.click()
+
+#Open linkedin for iitb links
 driver.get('https://www.linkedin.com/school/indian-institute-of-technology-bombay/people/')
 time.sleep(5)
 
+# Get scroll height
+last_height = driver.execute_script("return document.body.scrollHeight")
+
+while True:
+    # Scroll down to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight+1000);")
+
+    # Wait to load page
+    time.sleep(seconds)
+
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
 
 
 html = driver.page_source
@@ -30,6 +63,11 @@ print(type(soup))
 
 ##print(soup.find_all('a')[19].attrs)
 
-print(soup.find_all('a')[19]['href'])
-print('=====================================================')
-print(link['href'] for link in soup.find_all('a'))
+##Rishabh's link finder
+#print(soup.find_all('a')[19]['href'])
+#print('=====================================================')
+#print(link['href'] for link in soup.find_all('a'))
+
+##Pandas csv generator
+df = pandas.DataFrame(data={"Name": list_names, "Current Position": list_position, "Graduation Batch": list_batch, "Profile link": list_link})
+df.to_csv("./alumlinks.csv", sep=',',index=False)
