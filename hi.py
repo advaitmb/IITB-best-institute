2d28de6463f577887fa8b8c105128a64b2f4ebcd
Advait Bhat <advaitmb@gmail.com>
2020-01-28 22:08:06 +0530

    FInal Script

--- hi.py
+++ hi.py
@@ -1,14 +1,11 @@
-print(2+2)
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import random
+import pandas
 
-#lists
-list_names = []
-list_position = []
-list_batch = []
+#Link list
 list_link = []
 
 # A randomizer for the delay
@@ -27,18 +24,17 @@ username.send_keys('rishabhisar@gmail.com')
 #password
 password = driver.find_element_by_id('password')
 password.send_keys('puru@1997' + Keys.RETURN)
-## submit = driver.find_elements_by_class_name('btn__primary--large from__button--floating')
-##submit = driver.find_elements_by_tag_name('button')
-##submit.click()
 
 #Open linkedin for iitb links
 driver.get('https://www.linkedin.com/school/indian-institute-of-technology-bombay/people/')
 time.sleep(5)
 
-# Get scroll height
+#Get scroll height
 last_height = driver.execute_script("return document.body.scrollHeight")
 
-while True:
+#while True:
+dang = 1
+while dang < 5:
     # Scroll down to bottom
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight+1000);")
 
@@ -50,24 +46,19 @@ while True:
     if new_height == last_height:
         break
     last_height = new_height
-
+    dang = dang + 1;
 
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 print(type(soup))
-##print(soup.prettify())
 
-##linkedin_urls = driver.find_elements_by_id('ember297')
-##linkedin_urls = [url.text for url in linkedin_urls]
-##print(linkedin_urls[0])
-
-##print(soup.find_all('a')[19].attrs)
-
-##Rishabh's link finder
-#print(soup.find_all('a')[19]['href'])
-#print('=====================================================')
-#print(link['href'] for link in soup.find_all('a'))
+##Link Finder
+items = soup.find_all('a')
+for bla in items:
+    link = bla['href']
+    print(link)
+    list_link.append(link)
 
 ##Pandas csv generator
-df = pandas.DataFrame(data={"Name": list_names, "Current Position": list_position, "Graduation Batch": list_batch, "Profile link": list_link})
-df.to_csv("./alumlinks.csv", sep=',',index=False)
+df = pandas.DataFrame(data={"Profile link": list_link})
+df.to_csv("alumni.csv")
